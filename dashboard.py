44990967f664ba1d6751a963a8f56f455f6c9532
Thi-Ly Chu <doly90@gmail.com>
2023-11-26 16:57:46 +0100

    correct dashboard

--- dashboard.py
+++ dashboard.py
@@ -142,7 +142,7 @@ def main():
 
     # Load tweet data
     tweet_data = load_data()
-    url = "https://5b73-213-55-220-40.ngrok-free.app//predict"
+    url = "https://c054-213-55-220-81.ngrok-free.app//predict"
 
     # Sidebar navigation
     selected_page = st.sidebar.selectbox("Choisir une page", options=["EDA", "Analyse de Sentiment"])
